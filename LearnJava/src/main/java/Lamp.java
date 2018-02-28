b48f5a54bd7b157ff5484b3f0d70a153974101f2
kshtm <kshtm84@gmail.com>
2018-02-28 19:46:08 +0300

    Electricity

---
+++ LearnJava/src/main/java/Lamp.java
@@ -0,0 +1,10 @@
+public class Lamp implements ElectricityConsumer{
+
+    public void lightOn() {
+        System.out.println("Лампа зажглась");
+    }
+    @Override
+    public void electricityOn() {
+        lightOn();
+    }
+}
---
+++ LearnJava/src/main/java/Radio.java
@@ -0,0 +1,11 @@
+public class Radio implements ElectricityConsumer{
+
+    public void play() {
+        System.out.println("Радио играет");
+    }
+    @Override
+    public void electricityOn() {
+        play();
+    }
+
+}
